1ac7023c7b14f6753782d4c3bca840a4d8b20a31
Gly <gabin.lefranc@gmail.com>
2023-12-30 16:23:04 +0100

    Rename request/response to message in debug log

--- common/comm.rs
+++ common/comm.rs
@@ -1,7 +1,6 @@
 use byteorder::{BigEndian, WriteBytesExt, ReadBytesExt};
 use serde::{Serialize, de::DeserializeOwned};
 use std::io::{self, Write, Read};
-use futures::io::{AsyncWrite, AsyncRead};
 use interprocess::local_socket::{LocalSocketStream, tokio::LocalSocketStream as AsyncLocalSocketStream};
 use log::debug;
 
@@ -24,7 +23,7 @@ pub enum Error {
 pub fn send<T: Serialize>(stream: &mut LocalSocketStream, method: T) -> Result<(), Error> {
     use std::io::BufWriter;
     let req_str = serde_json::to_string(&method)?;
-    debug!("Sending request ({0} bytes): {req_str}", req_str.len());
+    debug!("Sending message ({0} bytes): {req_str}", req_str.len());
     let mut bytes = [0u8; 8];
     (&mut bytes as &mut [u8]).write_u64::<BigEndian>(req_str.len() as u64).map_err(Error::IntToBytes)?;
     // let mut stream = LocalSocketStream::connect(get_socket_name()).expect("Failed to connect to socket");
@@ -32,7 +31,7 @@ pub fn send<T: Serialize>(stream: &mut LocalSocketStream, method: T) -> Result<(
     writer.write_all(&bytes)?;
     writer.write_all(req_str.as_bytes())?;
     writer.flush()?;
-    debug!("Request sent");
+    debug!("Message sent");
     Ok(())
 }
 
@@ -40,13 +39,13 @@ pub fn receive<T: DeserializeOwned>(stream: &mut LocalSocketStream) -> Result<T,
     use std::io::BufReader;
     let mut reader = BufReader::new(stream);
     let mut bytes = [0u8; 8];
-    debug!("Receiving response size");
+    debug!("Receiving message size");
     reader.read_exact(&mut bytes)?;
     let len = io::Cursor::new(bytes).read_u64::<BigEndian>().map_err(Error::BytesToInt)?;
-    debug!("    Response size: {0} bytes", len);
+    debug!("    Message size: {0} bytes", len);
     let mut res_vec = vec![0; len as usize];
     reader.read_exact(&mut res_vec)?;
-    debug!("Response received: {}", String::from_utf8_lossy(&res_vec));
+    debug!("Message received: {}", String::from_utf8_lossy(&res_vec));
     let res: T = serde_json::from_slice(&res_vec)?;
     Ok(res)
 }
@@ -54,26 +53,26 @@ pub fn receive<T: DeserializeOwned>(stream: &mut LocalSocketStream) -> Result<T,
 pub async fn async_send<T: Serialize>(stream: &mut AsyncLocalSocketStream, method: T) -> Result<(), Error> {
     use futures::io::AsyncWriteExt;
     let req_str = serde_json::to_string(&method)?;
-    debug!("Sending request ({0} bytes): {req_str}", req_str.len());
+    debug!("Sending message ({0} bytes): {req_str}", req_str.len());
     let mut bytes = [0u8; 8];
     (&mut bytes as &mut [u8]).write_u64::<BigEndian>(req_str.len() as u64).map_err(Error::IntToBytes)?;
     stream.write_all(&bytes).await?;
     stream.write_all(req_str.as_bytes()).await?;
     stream.flush().await?;
-    debug!("Request sent");
+    debug!("Message sent");
     Ok(())
 }
 
 pub async fn async_receive<T: DeserializeOwned>(stream: &mut AsyncLocalSocketStream) -> Result<T, Error> {
     use futures::io::AsyncReadExt;
     let mut bytes = [0u8; 8];
-    debug!("Receiving response size");
+    debug!("Receiving message size");
     stream.read_exact(&mut bytes).await?;
     let len = io::Cursor::new(bytes).read_u64::<BigEndian>().map_err(Error::BytesToInt)?;
-    debug!("    Response size: {0} bytes", len);
+    debug!("    Message size: {0} bytes", len);
     let mut res_vec = vec![0; len as usize];
     stream.read_exact(&mut res_vec).await?;
-    debug!("Response received: {}", String::from_utf8_lossy(&res_vec));
+    debug!("Message received: {}", String::from_utf8_lossy(&res_vec));
     let res: T = serde_json::from_slice(&res_vec)?;
     Ok(res)
 }
